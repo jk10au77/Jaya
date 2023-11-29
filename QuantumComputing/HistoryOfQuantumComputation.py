f33785d6c5808ed9f31c2daba7d64594be5fde6c
jk10au77 <tatiparthi.jaya@outlook.com>
2023-11-29 18:55:39 +0530

    History of Quantum Computation

---
+++ QuantumComputing/HistoryOfQuantumComputation.py
@@ -0,0 +1,12 @@
+"""
+History of Quantum Computation and Qunatum Information:
+--------------------------------------------------------
+Q.  What is Quantum Mechanics?
+Ans:    Quantum Mechanics is a mathematical framework or set of rules for the construction of Physical theories.
+
+        For example:
+        -----------
+        1.  Quantum Electrodynamics: describes the interaction of atoms and light. And Quantum e=electrodynamcs is built up within the 
+            framework of quantum mechanics, but it contains specific rules not determined by quantum mechanics.
+
+"""
